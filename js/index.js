d3efd7bb112c9827d75716e2a6f95ab96c118d5e
Mykhailo <mykhailo.hoy@gmail.com>
2020-04-19 16:51:21 +0300

    amend

--- js/index.js
+++ js/index.js
@@ -30,6 +30,25 @@ if (width < 900) {
 }
 
 
+const supportsCSSTransformsOnSVG = (() => {
+  const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+  svg.setAttribute('viewBox', '0 0 2 2');
+  Object.assign(svg.style, {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: '2px',
+    height: '2px',
+  });
+  svg.innerHTML = '<rect width="1" height="1" style="transform: translate(1px, 1px)"/>';
+  document.body.appendChild(svg);
+  const result = document.elementFromPoint(1, 1) !== svg;
+  svg.parentNode.removeChild(svg);
+  return result;
+})();
+
+console.log(supportsCSSTransformsOnSVG);
+
 // handling form
 
 let sendBtn = document.querySelector(".contact-box__btn");
@@ -91,22 +110,3 @@ function revert() {
     buttonImage.setAttribute("alt", "надіслати");
   }
 }
-
-const supportsCSSTransformsOnSVG = (() => {
-  const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-  svg.setAttribute('viewBox', '0 0 2 2');
-  Object.assign(svg.style, {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    width: '2px',
-    height: '2px',
-  });
-  svg.innerHTML = '<rect width="1" height="1" style="transform: translate(1px, 1px)"/>';
-  document.body.appendChild(svg);
-  const result = document.elementFromPoint(1, 1) !== svg;
-  svg.parentNode.removeChild(svg);
-  return result;
-})();
-
-console.log(supportsCSSTransformsOnSVG);
