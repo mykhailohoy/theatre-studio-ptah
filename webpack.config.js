96f43e6fde4d88f29a295e218144c8ec1d1af200
Mykhailo <mykhailo.hoy@gmail.com>
2020-05-28 19:12:18 +0300

    amend

--- webpack.config.js
+++ webpack.config.js
@@ -27,11 +27,9 @@ module.exports = {
         use: [
           {
             loader: MiniCssExtractPlugin.loader,
-            // options: {
-            //   publicPath: (resourcePath, context) => {
-            //     return path.relative(path.dirname(resourcePath), context) + '/';
-            //   },
-            // },
+            options: {
+              publicPath: "./"
+            },
           },
           {
             loader: "css-loader"
